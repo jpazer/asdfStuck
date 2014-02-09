b5cb704d9d8f6aad46c6d8ebda717404f2bcd2b9
Katherine Whitlock <toroidalcode@gmail.com>
2014-02-09 13:51:38 -0500

    Public secret token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AsdfStuck::Application.config.secret_key_base = 'b15ccde13ad282b692c384e72174c8c377c46d49a7e31e746924881cc73e9835267b1fb11e76ecf37ee42aeb9c01ed22f9d2d7482658dc0f4af78b6d660719c6'
